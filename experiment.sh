7b6f909dbef79e64a76d999b53c91f8da0a54d79
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2020-01-15 13:08:57 +0200

    stuff

--- experiment.sh
+++ experiment.sh
@@ -3,7 +3,7 @@
 for i in {1..10}
 do
 	iptables -F
-	./pass alexa50.txt "$i\_noproxy"
+	./pass alexa50ro.txt "$i\_noproxy"
 	./proxyme.sh
-	./pass alexa50.txt "$i\_proxy"
+	./pass alexa50ro.txt "$i\_proxy"
 done
